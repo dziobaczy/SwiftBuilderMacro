e772b03841d66e53bfa1f6edc6feeea21b7a1481
Piotr Szadkowski <offthecode@gmail.com>
2023-06-11 22:37:18 +0200

    feat: add example macro usage

--- Sources/BuilderMacro/BuilderMacro.swift
+++ Sources/BuilderMacro/BuilderMacro.swift
@@ -1,6 +1,6 @@
 /// A macro that produces Builder
 @attached(member, names: arbitrary)
 public macro Builder() = #externalMacro(
-    module: "BuilderMacro",
+    module: "BuilderMacroMacros",
     type: "BuilderMacro"
 )
--- Sources/BuilderMacroClient/main.swift
+++ Sources/BuilderMacroClient/main.swift
@@ -1,2 +1,16 @@
 import BuilderMacro
+import Foundation
 
+// MARK: Example
+
+@Builder
+struct Breathing {
+    let uuid: UUID
+    let duration: Double
+    let thoughts: String?
+}
+
+let builder = Breathing.makeBuilder()
+builder.duration = 60
+
+print(String(describing: builder.build()))
