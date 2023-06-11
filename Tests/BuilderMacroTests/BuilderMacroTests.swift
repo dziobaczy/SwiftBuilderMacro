57697d414c6527950c861bdcf0a53d5ca9be6d80
Piotr Szadkowski <offthecode@gmail.com>
2023-06-12 00:25:07 +0200

    merge: remote gitignore

---
+++ Tests/BuilderMacroTests/BuilderMacroTests.swift
@@ -0,0 +1,34 @@
+import SwiftSyntaxMacros
+import SwiftSyntaxMacrosTestSupport
+import XCTest
+import BuilderMacroMacros
+
+let testMacros: [String: Macro.Type] = [
+    "stringify": StringifyMacro.self,
+]
+
+final class BuilderMacroTests: XCTestCase {
+    func testMacro() {
+        assertMacroExpansion(
+            """
+            #stringify(a + b)
+            """,
+            expandedSource: """
+            (a + b, "a + b")
+            """,
+            macros: testMacros
+        )
+    }
+
+    func testMacroWithStringLiteral() {
+        assertMacroExpansion(
+            #"""
+            #stringify("Hello, \(name)")
+            """#,
+            expandedSource: #"""
+            ("Hello, \(name)", #""Hello, \(name)""#)
+            """#,
+            macros: testMacros
+        )
+    }
+}
